80dde184a83eb8f90357c192bace2ab28f8318f4
Sxmurai <gavrw0607@gmail.com>
2021-03-20 12:47:27 -0500

    add some REST support (searching + decoding)

--- src/structures/Obsidian.ts
+++ src/structures/Obsidian.ts
@@ -1,16 +1,15 @@
 import { Socket, SocketOptions } from "./Socket";
 import { EventEmitter } from "events";
 import { Player } from "./Player";
-import { Structure } from "./Structure";
 
-const socket = Structure.get<Socket>("Socket");
-const player = Structure.get<Player>("Player");
+import http from "http";
+import https from "https";
 
 export class Obsidian extends EventEmitter {
   public options: ObsidianOptions;
 
-  public players: Map<string, typeof player> = new Map();
-  public sockets: Map<string, typeof socket> = new Map();
+  public players: Map<string, Player> = new Map();
+  public sockets: Map<string, Socket> = new Map();
 
   public constructor(options: ObsidianOptions) {
     super();
@@ -69,6 +68,86 @@ export class Obsidian extends EventEmitter {
     this.players.get(guild)?.disconnect();
     this.players.delete(guild);
   }
+
+  /**
+   * Search for something and return tracks
+   * @param {string} search
+   * @param {?Socket} socket
+   * @returns {Promise<ObsidianTrackResponse>}
+   */
+  public async search(
+    search: string,
+    socket?: Socket
+  ): Promise<ObsidianTrackResponse> {
+    // TODO: make better socket selection if none provided
+    socket = socket ?? [...this.sockets.values()][0];
+
+    return new Promise((res, rej) => {
+      const { options } = socket!;
+      const { request } = options.secure ? https : http;
+
+      request(
+        `http${options.secure ? "s" : ""}://${options.address}:${
+          options.port
+        }/loadtracks?identifier=${encodeURIComponent(search)}`,
+        {
+          headers: {
+            Authorization: options.password,
+          },
+        },
+        (response) => {
+          const data = Buffer.alloc(0);
+
+          response
+            .on("data", (c) => Buffer.concat([data, c]))
+            .on("error", (error) => rej(error))
+            .on("end", () => res(JSON.parse(data.toString())));
+        }
+      )
+        .on("error", (error) => rej(error))
+        .end();
+    });
+  }
+
+  /**
+   * Returns a JSON response from a base64 string
+   * @param {string} track
+   * @param {?Socket} socket
+   * @returns {Promise<ObsidianTrackResponseTracks>}
+   */
+  public async decode(
+    track: string,
+    socket?: Socket
+  ): Promise<ObsidianTrackResponseTracks> {
+    // TODO: make better socket selection if none provided
+    socket = socket ?? [...this.sockets.values()][0];
+
+    return new Promise((res, rej) => {
+      const { options } = socket!;
+      const { request } = options.secure ? https : http;
+
+      request(
+        `http${options.secure ? "s" : ""}://${options.address}:${
+          options.port
+        }/decodetrack?track=${encodeURIComponent(track)}`,
+        {
+          headers: {
+            Authorization: options.password,
+          },
+        },
+        (response) => {
+          const data = Buffer.alloc(0);
+
+          response
+            .on("data", (c) => Buffer.concat([data, c]))
+            .on("error", (error) => rej(error))
+            .on("end", () => res(JSON.parse(data.toString())));
+        }
+      )
+        .on("error", (error) => rej(error))
+        .end();
+    });
+  }
 }
 
 export interface ObsidianOptions {
@@ -81,3 +160,43 @@ export interface PlayerCreateOptions {
   guild: string;
   socket?: Socket;
 }
+
+export interface ObsidianTrackResponse {
+  load_type: ObsidianTrackResponseLoadtype;
+  playlist_info?: ObsidianTrackResponseTrackPlaylist;
+  tracks: ObsidianTrackResponseTracks[];
+  exception?: ObsidianTrackResponseTrackException;
+}
+
+export interface ObsidianTrackResponseTrackPlaylist {
+  name: string;
+  selectedTrack: number;
+}
+
+export interface ObsidianTrackResponseTracks {
+  track: string;
+  info: ObsidianTrackResponseTrackInfo;
+}
+
+export interface ObsidianTrackResponseTrackException {
+  message: string;
+  severity: "COMMON" | "SUSPICIOUS" | "FAULT";
+}
+
+export interface ObsidianTrackResponseTrackInfo {
+  title: string;
+  author: string;
+  uri: string;
+  identifier: string;
+  length: number;
+  position: number;
+  is_stream: boolean;
+  is_seekable: boolean;
+}
+
+export type ObsidianTrackResponseLoadtype =
+  | "NO_MATCHES"
+  | "LOAD_FAILED"
+  | "TRACK_LOADED"
+  | "SEARCH_RESULT"
+  | "PLAYLIST_LOADED";
