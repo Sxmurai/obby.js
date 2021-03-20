53572834beb0aa926e74a0dc7ea98a0185091f18
Sxmurai <gavrw0607@gmail.com>
2021-03-20 15:01:33 -0500

    add event typings

--- typings/structures/Obsidian.d.ts
+++ typings/structures/Obsidian.d.ts
@@ -2,52 +2,75 @@
 import { Socket, SocketOptions } from "./Socket";
 import { EventEmitter } from "events";
 import { Player } from "./Player";
+
 export declare class Obsidian extends EventEmitter {
-    options: ObsidianOptions;
-    players: Map<string, Player>;
-    sockets: Map<string, Socket>;
-    constructor(options: ObsidianOptions);
-    init(id?: string): void;
-    create(options: PlayerCreateOptions): Player | undefined;
-    destroy(guild: string): void;
-    search(search: string, socket?: Socket): Promise<ObsidianTrackResponse>;
-    decode(track: string, socket?: Socket): Promise<ObsidianTrackResponseTracks>;
+  options: ObsidianOptions;
+  players: Map<string, Player>;
+  sockets: Map<string, Socket>;
+  constructor(options: ObsidianOptions);
+  init(id?: string): void;
+  create(options: PlayerCreateOptions): Player | undefined;
+  destroy(guild: string): void;
+  search(search: string, socket?: Socket): Promise<ObsidianTrackResponse>;
+  decode(track: string, socket?: Socket): Promise<ObsidianTrackResponseTracks>;
+
+  on(event: "connected", listener: (socket: Socket) => any);
+  on(
+    event: "raw",
+    listener: (socket: Socket, opCode: number, data: any) => any
+  );
+  on(
+    event: "closed",
+    listener: (socket: Socket, code: number, reason?: string) => any
+  );
+  on(event: "error", listener: (socket: Socket, error: Error) => any);
 }
+
 export interface ObsidianOptions {
-    nodes: SocketOptions[];
-    id?: string;
-    send: (id: string, payload: any) => any;
+  nodes: SocketOptions[];
+  id?: string;
+  send: (id: string, payload: any) => any;
 }
 export interface PlayerCreateOptions {
-    guild: string;
-    socket?: Socket;
+  guild: string;
+  socket?: Socket;
 }
 export interface ObsidianTrackResponse {
-    load_type: ObsidianTrackResponseLoadtype;
-    playlist_info?: ObsidianTrackResponseTrackPlaylist;
-    tracks: ObsidianTrackResponseTracks[];
-    exception?: ObsidianTrackResponseTrackException;
+  load_type: ObsidianTrackResponseLoadtype;
+  playlist_info?: ObsidianTrackResponseTrackPlaylist;
+  tracks: ObsidianTrackResponseTracks[];
+  exception?: ObsidianTrackResponseTrackException;
 }
+
 export interface ObsidianTrackResponseTrackPlaylist {
-    name: string;
-    selectedTrack: number;
+  name: string;
+  selectedTrack: number;
 }
+
 export interface ObsidianTrackResponseTracks {
-    track: string;
-    info: ObsidianTrackResponseTrackInfo;
+  track: string;
+  info: ObsidianTrackResponseTrackInfo;
 }
+
 export interface ObsidianTrackResponseTrackException {
-    message: string;
-    severity: "COMMON" | "SUSPICIOUS" | "FAULT";
+  message: string;
+  severity: "COMMON" | "SUSPICIOUS" | "FAULT";
 }
+
 export interface ObsidianTrackResponseTrackInfo {
-    title: string;
-    author: string;
-    uri: string;
-    identifier: string;
-    length: number;
-    position: number;
-    is_stream: boolean;
-    is_seekable: boolean;
+  title: string;
+  author: string;
+  uri: string;
+  identifier: string;
+  length: number;
+  position: number;
+  is_stream: boolean;
+  is_seekable: boolean;
 }
-export declare type ObsidianTrackResponseLoadtype = "NO_MATCHES" | "LOAD_FAILED" | "TRACK_LOADED" | "SEARCH_RESULT" | "PLAYLIST_LOADED";
+
+export declare type ObsidianTrackResponseLoadtype =
+  | "NO_MATCHES"
+  | "LOAD_FAILED"
+  | "TRACK_LOADED"
+  | "SEARCH_RESULT"
+  | "PLAYLIST_LOADED";
--- typings/structures/Player.d.ts
+++ typings/structures/Player.d.ts
@@ -2,99 +2,127 @@
 import { EventEmitter } from "events";
 import { Obsidian, PlayerCreateOptions } from "./Obsidian";
 import { OpCodes, Socket } from "./Socket";
+
 export declare class Player extends EventEmitter {
-    #private;
-    socket: Socket;
-    guild: string;
-    channel: string | null;
-    stats: PlayerStats;
-    track: string;
-    playing: boolean;
-    paused: boolean;
-    private _state;
-    private _server;
-    constructor(obsidian: Obsidian, options: PlayerCreateOptions);
-    handleVoice(packet: any): void;
-    connect(channel: string, options?: ConnectOptions): void;
-    disconnect(): void;
-    play(track: string, options?: PlayTrackOptions): void;
-    stop(): void;
-    seek(position: number): void;
-    pause(): void;
-    resume(): void;
-    filters(data: PlayerFilterData): void;
-    send(op: OpCodes, payload?: any): void;
+  #private;
+  socket: Socket;
+  guild: string;
+  channel: string | null;
+  stats: PlayerStats;
+  track: string;
+  playing: boolean;
+  paused: boolean;
+  private _state;
+  private _server;
+  constructor(obsidian: Obsidian, options: PlayerCreateOptions);
+  handleVoice(packet: any): void;
+  connect(channel: string, options?: ConnectOptions): void;
+  disconnect(): void;
+  play(track: string, options?: PlayTrackOptions): void;
+  stop(): void;
+  seek(position: number): void;
+  pause(): void;
+  resume(): void;
+  filters(data: PlayerFilterData): void;
+  send(op: OpCodes, payload?: any): void;
+
+  on(event: "start", listener: ({ track }: { track: string }) => any);
+  on(
+    event: "end",
+    listener: ({ track, reason }: { track: string; reason: string }) => any
+  );
+  on(
+    event: "stuck",
+    listener: ({
+      track,
+      threshold_ms,
+    }: {
+      track: string;
+      threshold_ms: number;
+    }) => any
+  );
+  on(
+    event: "exception",
+    listener: ({
+      track,
+      exception,
+    }: {
+      track: string;
+      exception: { message: string; cause: string; severity: string };
+    }) => any
+  );
 }
+
 export interface PlayerStats {
-    frames: {
-        lost: number;
-        sent: number;
-    };
-    playingTrack: {
-        track: string;
-        position: number;
-        paused: boolean;
-    };
+  frames: {
+    lost: number;
+    sent: number;
+  };
+  playingTrack: {
+    track: string;
+    position: number;
+    paused: boolean;
+  };
 }
 export interface ConnectOptions {
-    deaf?: boolean;
-    mute?: boolean;
+  deaf?: boolean;
+  mute?: boolean;
 }
 export interface PlayTrackOptions {
-    end?: number;
-    start?: number;
-    noReplace?: boolean;
+  end?: number;
+  start?: number;
+  noReplace?: boolean;
 }
 export interface PlayerFilterData {
-    volume?: number;
-    tremolo?: PlayerFilterDataTremolo;
-    equalizer?: PlayerFilterDataEqualizerBands[];
-    timescale?: PlayerFilterDataTimescale;
-    karaoke?: PlayerFilterDataKaraoke;
-    channel_mix?: PlayerFilterDataChannelMix;
-    vibrato?: PlayerFilterDataVibrato;
-    rotation?: PlayerFilterDataRotation;
-    low_pass?: PlayerFilterDataLowPass;
+  volume?: number;
+  tremolo?: PlayerFilterDataTremolo;
+  equalizer?: PlayerFilterDataEqualizerBands[];
+  timescale?: PlayerFilterDataTimescale;
+  karaoke?: PlayerFilterDataKaraoke;
+  channel_mix?: PlayerFilterDataChannelMix;
+  vibrato?: PlayerFilterDataVibrato;
+  rotation?: PlayerFilterDataRotation;
+  low_pass?: PlayerFilterDataLowPass;
 }
 export interface PlayerFilterDataTremolo {
-    frequency: number;
-    depth: number;
+  frequency: number;
+  depth: number;
 }
 export interface PlayerFilterDataTimescale {
-    pitch: number;
-    pitch_octaves?: number;
-    picth_semi_tones?: number;
-    rate: number;
-    rate_change?: number;
-    speed?: number;
-    speed_change?: number;
+  pitch: number;
+  pitch_octaves?: number;
+  picth_semi_tones?: number;
+  rate: number;
+  rate_change?: number;
+  speed?: number;
+  speed_change?: number;
 }
 export interface PlayerFilterDataKaraoke {
-    filter_band?: number;
-    filter_width?: number;
-    level: number;
-    mono_level?: number;
+  filter_band?: number;
+  filter_width?: number;
+  level: number;
+  mono_level?: number;
 }
 export interface PlayerFilterDataChannelMix {
-    right_to_left: number;
-    right_to_right: number;
-    left_to_right: number;
-    left_to_left: number;
+  right_to_left: number;
+  right_to_right: number;
+  left_to_right: number;
+  left_to_left: number;
 }
 export interface PlayerFilterDataVibrato {
-    frequency: number;
-    depth: number;
+  frequency: number;
+  depth: number;
 }
 export interface PlayerFilterDataRotation {
-    rotation_hz: number;
+  rotation_hz: number;
 }
 export interface PlayerFilterDataLowPass {
-    smoothing: number;
+  smoothing: number;
 }
 export interface EqualizerBands {
-    band: number;
-    gain: number;
+  band: number;
+  gain: number;
 }
 export interface PlayerFilterDataEqualizerBands {
-    bands: EqualizerBands[];
+  bands: EqualizerBands[];
 }
