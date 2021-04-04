fa7ebf2e8ef070d99689a062b7910cffe56f9a26
Sxmurai <gavrw0607@gmail.com>
2021-04-04 11:27:22 -0500

    clean slate for rewriting

--- src/structures/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./Obsidian";
-export * from "./Player";
-export * from "./Socket";
-export * from "./Plugin";
--- typings/structures/Plugin.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Obsidian } from "./Obsidian";
-export declare abstract class Plugin {
-    obsidian: Obsidian;
-    loaded: boolean;
-    manualLoad: boolean;
-    load(obsidian: Obsidian): void;
-    init(): void;
-}
